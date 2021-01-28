ea98326cc15f2e1215a507baa3c95fee2373231b
ross-dotcom <wross843@gmail.com>
2021-01-28 20:50:33 +0200

    Learning how to use Tkinter. Created a window, added Label, Button, Entry, Text and Frame widgets.

--- gui.py
+++ gui.py
@@ -1,7 +1,31 @@
 import tkinter as tk
 
 window = tk.Tk() #Create an instance of the Tkinter class.
-greeting = tk.Label(text='Hello, World!') #Create a 'Label' widget with the text "Hello, World!" and assign it to the variable 'greeting'.
+
+greeting = tk.Label(text='\nMy GUI Application\n')
+
+button = tk.Button(text='\nClick\n')
+
+label = tk.Label(text='\nName')
+
+entry = tk.Entry()
+entry.insert(0, "Your Name Here...")
+
+about = tk.Label(text='\nTell me about yourself')
+
+text = tk.Text()
+text.insert("1.0", "Type...")
+
 greeting.pack() #Add widget to the window.
+button.pack()
+label.pack()
+entry.pack()
+about.pack()
+text.pack()
+    
+frame = tk.Frame(master=window, relief=tk.RIDGE, borderwidth=10)
+frame.pack()
+frame_label = tk.Label(master=frame, text="FRAME")
+frame_label.pack()
 
 window.mainloop() #Tells Python to run the Tkinter event loop.
